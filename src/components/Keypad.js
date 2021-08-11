0f7f2efc2708ba701c1cadff72cab6332676c77b
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-08-11 13:22:16 +1000

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,37 @@
-// Code Keypad Component Here
+import React, {Component} from 'react'
+
+export default class Keypad extends Component{
+    
+    keyUpEvent = () => (console.log("Entering password"))
+
+    render(){
+        return(
+            <div>
+                <input type = "password" onKeyUp= {this.keyUpEvent}></input>
+            </div>
+        )
+    }
+
+}
+
+/* import React from 'react';
+
+class Keypad extends React.Component {
+
+  handleInputPassword = () => console.log('Entering password...')
+
+  render() {
+    return (
+      <div>
+        <input 
+          type="password"
+          onKeyUp={this.handleInputPassword} 
+        />
+        <button>Submit</button>
+      </div>
+    )
+  }
+}
+
+export default Keypad; 
+ */
